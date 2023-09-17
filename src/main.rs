3d2c44b5afed2b36f08c8ae0bd060a3b61f2236d
Кирилл Сысоев <k.sysoev@me.com>
2023-09-17 23:10:36 +0800

    Implementst draft of socks5 proxy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,116 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Read;
+use std::io::Write;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
+use std::net::ToSocketAddrs;
+use std::net::{TcpListener, TcpStream};
+
+const ADDRESS: &str = "127.0.0.1:1080";
+
+fn handle_client(mut stream: TcpStream) {
+    println!("New client: {}", stream.peer_addr().unwrap());
+
+    let mut buf = [0u8; 2];
+    stream.read_exact(&mut buf).unwrap();
+
+    if buf[0] != 0x05 {
+        println!("Invalid SOCKS5 version");
+        return;
+    }
+
+    let num_methods = buf[1] as usize;
+    println!("SOCKS5 number methods: ({:?})", num_methods);
+
+    let mut methods = vec![0; num_methods];
+    for _ in 0..num_methods {
+        let mut buf = [0u8; 1];
+        println!("SOCKS5 reading method");
+        stream.read_exact(&mut buf).unwrap();
+        println!("SOCKS5 method: ({:?})", buf);
+        methods.push(buf[0]);
+    }
+
+    println!("SOCKS5 methods: ({:?})", methods);
+    if !methods.contains(&0x00) {
+        println!("No supported authentication methods");
+        return;
+    }
+
+    // send the SOCKS5 handshake response
+    stream.write(&[0x05, 0x00]).unwrap();
+    stream.flush().unwrap();
+
+    // read the SOCKS5 request
+    let mut buf = [0; 4];
+    stream.read_exact(&mut buf).unwrap();
+
+    println!("SOCKS5 request: ({:?})", buf);
+
+    // check the SOCKS5 version and command
+    if buf[0] != 0x05 {
+        println!("Invalid SOCKS5 version");
+        return;
+    }
+
+    if buf[1] != 0x01 {
+        println!("Unsupported SOCKS5 command");
+        return;
+    }
+
+    // handle the SOCKS5 request
+    let addr = match buf[3] {
+        0x01 => {
+            let mut buf = [0; 4];
+            stream.read_exact(&mut buf).unwrap();
+            IpAddr::V4(Ipv4Addr::from(buf))
+        }
+        0x04 => {
+            let mut buf = [0; 16];
+            stream.read_exact(&mut buf).unwrap();
+            IpAddr::V6(Ipv6Addr::from(buf))
+        }
+        0x03 => {
+            let mut buf = [0; 1];
+            stream.read_exact(&mut buf).unwrap();
+            let len = buf[0] as usize;
+            let mut buf = vec![0; len];
+            stream.read_exact(&mut buf).unwrap();
+            let domain = String::from_utf8_lossy(&buf);
+            let mut addrs = (domain.as_ref(), 0).to_socket_addrs().unwrap();
+            addrs.next().unwrap().ip()
+        }
+        _ => {
+            println!("Invalid SOCKS5 address type");
+            return;
+        }
+    };
+
+    let mut buf = [0; 2];
+    stream.read_exact(&mut buf).unwrap();
+    let port = u16::from_be_bytes(buf);
+
+    println!("SOCKS5 request: {}:{} ({:?})", addr, port, buf);
+
+    // send the SOCKS5 request response
+    stream
+        .write(&[0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0])
+        .unwrap();
+    stream.flush().unwrap();
+
+    // read the SOCKS5 data
+    let mut buf = [0; 128];
+    stream.read(&mut buf).unwrap();
+
+    println!("SOCKS5 data: {:?}", buf);
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind(ADDRESS)?;
+
+    // accept connections and process them serially
+    for stream in listener.incoming() {
+        handle_client(stream?);
+    }
+    Ok(())
 }
