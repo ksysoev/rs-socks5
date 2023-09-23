2a0142a222c2642051d7b6a4f5675e546182fa61
Кирилл Сысоев <k.sysoev@me.com>
2023-09-23 22:08:09 +0800

    Replace blocking io with tokio async io

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,35 @@
 use env_logger;
-use log::{debug, trace};
-use std::io::{Read, Write};
+
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
 use std::net::ToSocketAddrs;
-use std::net::{TcpListener, TcpStream};
+
+use log::{debug, trace};
+use std::io;
+use tokio::net::{TcpListener, TcpStream};
 
 const ADDRESS: &str = "127.0.0.1:1080";
 
-fn handle_client(mut stream: TcpStream) {
-    trace!("New client: {}", stream.peer_addr().unwrap());
+#[tokio::main]
+async fn main() {
+    env_logger::init();
 
+    let listener = TcpListener::bind(ADDRESS).await.unwrap();
+
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        // A new task is spawned for each inbound socket. The socket is
+        // moved to the new task and processed there.
+        tokio::spawn(async move {
+            process(socket).await;
+        });
+    }
+}
+
+async fn process(stream: TcpStream) {
     let mut buf = [0u8; 2];
-    stream.read_exact(&mut buf).unwrap();
+    read_exact(&stream, &mut buf).await.unwrap();
 
     if buf[0] != 0x05 {
         trace!("Invalid SOCKS5 version");
@@ -25,7 +41,7 @@ fn handle_client(mut stream: TcpStream) {
     let mut methods = vec![0; num_methods];
     for _ in 0..num_methods {
         let mut buf = [0u8; 1];
-        stream.read_exact(&mut buf).unwrap();
+        read_exact(&stream, &mut buf).await.unwrap();
         methods.push(buf[0]);
     }
 
@@ -35,12 +51,11 @@ fn handle_client(mut stream: TcpStream) {
     }
 
     // send the SOCKS5 handshake response
-    stream.write(&[0x05, 0x00]).unwrap();
-    stream.flush().unwrap();
+    write_all(&stream, &[0x05, 0x00]).await.unwrap();
 
     // read the SOCKS5 request
     let mut buf = [0; 4];
-    stream.read_exact(&mut buf).unwrap();
+    read_exact(&stream, &mut buf).await.unwrap();
 
     // check the SOCKS5 version and command
     if buf[0] != 0x05 {
@@ -57,20 +72,20 @@ fn handle_client(mut stream: TcpStream) {
     let addr = match buf[3] {
         0x01 => {
             let mut buf = [0; 4];
-            stream.read_exact(&mut buf).unwrap();
+            read_exact(&stream, &mut buf).await.unwrap();
             IpAddr::V4(Ipv4Addr::from(buf))
         }
         0x04 => {
             let mut buf = [0; 16];
-            stream.read_exact(&mut buf).unwrap();
+            read_exact(&stream, &mut buf).await.unwrap();
             IpAddr::V6(Ipv6Addr::from(buf))
         }
         0x03 => {
             let mut buf = [0; 1];
-            stream.read_exact(&mut buf).unwrap();
+            read_exact(&stream, &mut buf).await.unwrap();
             let len = buf[0] as usize;
             let mut buf = vec![0; len];
-            stream.read_exact(&mut buf).unwrap();
+            read_exact(&stream, &mut buf).await.unwrap();
             let domain = String::from_utf8_lossy(&buf);
             let mut addrs = (domain.as_ref(), 0).to_socket_addrs().unwrap();
             addrs.next().unwrap().ip()
@@ -82,70 +97,104 @@ fn handle_client(mut stream: TcpStream) {
     };
 
     let mut buf = [0; 2];
-    stream.read_exact(&mut buf).unwrap();
+    read_exact(&stream, &mut buf).await.unwrap();
     let port = u16::from_be_bytes(buf);
 
     debug!("SOCKS5 request: {}:{} ({:?})", addr, port, buf);
 
-    let mut dest_stream = match TcpStream::connect((addr, port)) {
+    let dest_stream = match TcpStream::connect((addr, port)).await {
         Ok(stream) => stream,
         Err(e) => {
             debug!("Failed to connect to destination: {}", e);
-            stream
-                .write(&[0x05, 0x05, 0x00, 0x01, 0, 0, 0, 0, 0, 0])
+
+            write_all(&stream, &[0x05, 0x05, 0x00, 0x01, 0, 0, 0, 0, 0, 0])
+                .await
                 .unwrap();
-            stream.flush().unwrap();
             return;
         }
     };
 
-    // send the SOCKS5 request response
-    stream
-        .write(&[0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0])
+    write_all(&stream, &[0x05, 0x00, 0x00, 0x01, 0, 0, 0, 0, 0, 0])
+        .await
         .unwrap();
-    stream.flush().unwrap();
 
     loop {
-        let mut client_buf = [0; 4096];
-        match stream.read(&mut client_buf) {
-            Ok(n) => {
-                if n == 0 {
-                    break;
-                }
-                dest_stream.write_all(&client_buf[0..n]).unwrap();
-                dest_stream.flush().unwrap();
-            }
-            Err(e) => {
-                debug!("Failed to read from client: {}", e);
-                break;
-            }
-        }
+        tokio::select! {
+            _ = stream.readable() => {
+                let mut buf = [0; 4096];
 
-        let mut dest_buf = [0; 4096];
-
-        match dest_stream.read(&mut dest_buf) {
-            Ok(n) => {
-                if n == 0 {
-                    break;
+                match stream.try_read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        write_all(&dest_stream, &buf[0..n]).await.unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
+                    }
+                    Err(_) => {
+                        break;
+                    }
                 }
-                stream.write_all(&dest_buf[0..n]).unwrap();
-                stream.flush().unwrap();
             }
-            Err(e) => {
-                debug!("Failed to read from destination: {}", e);
-                break;
+            _ = dest_stream.readable() => {
+                let mut buf = [0; 4096];
+
+                match dest_stream.try_read(&mut buf) {
+                    Ok(0) => break,
+                    Ok(n) => {
+                        write_all(&stream, &buf[0..n]).await.unwrap();
+                    }
+                    Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                        continue;
+                    }
+                    Err(_) => {
+                        break;
+                    }
+                }
             }
         }
     }
+    debug!("Connection closed");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    env_logger::init();
-    let listener = TcpListener::bind(ADDRESS)?;
+async fn read_exact(stream: &TcpStream, buf: &mut [u8]) -> std::io::Result<()> {
+    let mut offset = 0;
+    while offset < buf.len() {
+        stream.readable().await?;
 
-    // accept connections and process them serially
-    for stream in listener.incoming() {
-        handle_client(stream?);
+        match stream.try_read(&mut buf[offset..]) {
+            Ok(0) => break,
+            Ok(n) => {
+                offset += n;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn write_all(stream: &TcpStream, buf: &[u8]) -> std::io::Result<()> {
+    let mut offset = 0;
+    while offset < buf.len() {
+        stream.writable().await?;
+
+        match stream.try_write(&buf[offset..]) {
+            Ok(0) => break,
+            Ok(n) => {
+                offset += n;
+            }
+            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
+                continue;
+            }
+            Err(e) => {
+                return Err(e.into());
+            }
+        }
     }
     Ok(())
 }
