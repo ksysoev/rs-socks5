2850173fa5316cd0383f9d3de1073e4be4ac7bc5
Кирилл Сысоев <k.sysoev@me.com>
2023-09-24 11:44:56 +0800

    Creates server abstruction

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
 use env_logger;
-use tokio::net::TcpListener;
 
-const ADDRESS: &str = "127.0.0.1:1080";
+const ADDRESS: &str = "127.0.0.1";
+const PORT: u16 = 1080;
+
+use socks5::server::SOCKS5Server;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let listener = TcpListener::bind(ADDRESS).await.unwrap();
-
-    loop {
-        let (socket, _) = listener.accept().await.unwrap();
-        // A new task is spawned for each inbound socket. The socket is
-        // moved to the new task and processed there.
-        tokio::spawn(async move {
-            socks5::process(socket).await;
-        });
-    }
+    SOCKS5Server::new(ADDRESS.to_string(), PORT).run().await;
 }
---
+++ src/server.rs
@@ -0,0 +1,26 @@
+use tokio::net::TcpListener;
+
+mod connection;
+
+pub struct SOCKS5Server {
+    address: String,
+    port: u16,
+}
+
+impl SOCKS5Server {
+    pub fn new(address: String, port: u16) -> Self {
+        SOCKS5Server { address, port }
+    }
+
+    pub async fn run(&self) {
+        let address = format!("{}:{}", self.address, self.port);
+        let listener = TcpListener::bind(address).await.unwrap();
+
+        loop {
+            let (socket, _) = listener.accept().await.unwrap();
+            tokio::spawn(async move {
+                connection::process(socket).await;
+            });
+        }
+    }
+}
